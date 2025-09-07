5bcafc7c463e4de32bcee7b076d49ca260e03422
Mosammat Samiea Jahan Eva <137921062+Evaliuna@users.noreply.github.com>
2025-09-07 09:44:49 +0600

    Create 339A.cpp

---
+++ 339A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    
+    string digits;
+    for(char c: s) if(isdigit(c)) digits.push_back(c);
+    
+    sort(digits.begin(), digits.end());
+    
+    int j =0;
+    for(char &c : s){
+        if(isdigit(c)) c = digits[j++];
+    }
+    
+    cout<<s<<"\n";
+    
+    return 0;
+}
